9920869933edc167c6d2e618d0b0cad5353d6a10
Alan Nguyen <alan.t.ngn@gmail.com>
2019-02-20 18:31:50 +0100

    Disable product page for now

--- src/templates/ProductPage.js
+++ src/templates/ProductPage.js
@@ -7,27 +7,27 @@ import ProductAttributes from '../components/ProductAttributes'
 
 class ProductPageTemplate extends React.PureComponent {
   render() {
-    const productInfo = get(this, 'props.data.allMoltinProduct')
-    const data = productInfo.edges[0].node
-    const slug = data.slug
-    const image = get(data, 'includedData.main_image.link.href')
-    const sizes = get(data, 'mainImage.childImageSharp.sizes')
-    const product = {
-      ...data,
-      id: data.originalId,
-      image,
-      mainImage: data.mainImage,
-      header: data.name,
-      meta: data.meta,
-      sku: data.sku,
-    }
+    // const productInfo = get(this, 'props.data.allMoltinProduct')
+    // const data = productInfo.edges[0].node
+    // const slug = data.slug
+    // const image = get(data, 'includedData.main_image.link.href')
+    // const sizes = get(data, 'mainImage.childImageSharp.sizes')
+    // const product = {
+    //   ...data,
+    //   id: data.originalId,
+    //   image,
+    //   mainImage: data.mainImage,
+    //   header: data.name,
+    //   meta: data.meta,
+    //   sku: data.sku,
+    // }
 
     if(!sizes) return null
 
     return (
       <div>
         <Helmet title={slug} />
-        <ProductSummary {...product} />
+        {/*<ProductSummary {...product} />*/}
       </div>
     )
   }
@@ -35,41 +35,41 @@ class ProductPageTemplate extends React.PureComponent {
 
 export default ProductPageTemplate
 
-export const pageQuery = graphql`
-  query ProductsQuery($originalId: String!) {
-    allMoltinProduct(filter: { originalId: { eq: $originalId } }) {
-      edges {
-        node {
-          originalId
-          name
-          description
-          meta {
-            display_price {
-              with_tax {
-                amount
-                currency
-                formatted
-              }
-            }
-          }
-          includedData {
-            main_image {
-              link {
-                href
-              }
-            }
-          }
-          mainImage {
-            childImageSharp {
-              sizes(maxWidth: 400) {
-                ...GatsbyImageSharpSizes
-              }
-            }
-          }
-          slug
-          sku
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query ProductsQuery($originalId: String!) {
+//     allMoltinProduct(filter: { originalId: { eq: $originalId } }) {
+//       edges {
+//         node {
+//           originalId
+//           name
+//           description
+//           meta {
+//             display_price {
+//               with_tax {
+//                 amount
+//                 currency
+//                 formatted
+//               }
+//             }
+//           }
+//           includedData {
+//             main_image {
+//               link {
+//                 href
+//               }
+//             }
+//           }
+//           mainImage {
+//             childImageSharp {
+//               sizes(maxWidth: 400) {
+//                 ...GatsbyImageSharpSizes
+//               }
+//             }
+//           }
+//           slug
+//           sku
+//         }
+//       }
+//     }
+//   }
+// `
